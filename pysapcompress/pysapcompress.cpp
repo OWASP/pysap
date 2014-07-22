9cd36081da4ad5c0eec8d1fbdc6aba0f8367ad7b
Martin Gallo <mgallo@coresecurity.com>
2014-07-22 19:12:11 -0300

    Code cleanup and license notice format update

--- pysapcompress/pysapcompress.cpp
+++ pysapcompress/pysapcompress.cpp
@@ -1,24 +1,24 @@
 /*
-## ===========
-## pysapcompress - SAP Compression library wrapper for Python
-##
-## Copyright (C) 2014 Core Security Technologies
-##
-## The library was designed and developed by Martin Gallo of Core Security
-## Technologies - Security Consulting Services
-##
-## Based on the work performed by Dennis Yurichev <dennis@conus.info>
-##
-## This program is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License
-## as published by the Free Software Foundation; either version 2
-## of the License, or (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##==============
+# ===========
+# pysapcompress - SAP Compression library wrapper for Python
+#
+# Copyright (C) 2014 Core Security Technologies
+#
+# The library was designed and developed by Martin Gallo of Core Security
+# Technologies - Security Consulting Services
+#
+# Based on the work performed by Dennis Yurichev <dennis@conus.info>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ==============
 */
 
 #include <Python.h>
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-## ===========
-## pysap - Python library for crafting SAP's network protocols packets
-##
-## Copyright (C) 2014 Core Security Technologies
-##
-## The library was designed and developed by Martin Gallo from the Security
-## Consulting Services team of Core Security Technologies.
-##
-## This program is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License
-## as published by the Free Software Foundation; either version 2
-## of the License, or (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##==============
+# ===========
+# pysap - Python library for crafting SAP's network protocols packets
+#
+# Copyright (C) 2014 Core Security Technologies
+#
+# The library was designed and developed by Martin Gallo from the Security
+# Consulting Services team of Core Security Technologies.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ==============
 
 # Standard imports
 from os import path, makedirs, system
@@ -56,20 +56,20 @@ class APIDocumentationCommand(Command):
 
 
 sapcompress = Extension('pysapcompress',
-                         ['pysapcompress/pysapcompress.cpp',
-                          'pysapcompress/vpa105CsObjInt.cpp',
-                          'pysapcompress/vpa106cslzc.cpp',
-                          'pysapcompress/vpa107cslzh.cpp',
-                          'pysapcompress/vpa108csulzh.cpp'],
-                         define_macros=[  # Enable this macro if you want some debugging information on the (de)compression functions
-                                        #('DEBUG', None),
-                                          # Enable this macro if you want detailed debugging information (hexdumps) on the (de)compression functions
-                                        #('DEBUG_TRACE', None),
-                                        ])
+                        ['pysapcompress/pysapcompress.cpp',
+                         'pysapcompress/vpa105CsObjInt.cpp',
+                         'pysapcompress/vpa106cslzc.cpp',
+                         'pysapcompress/vpa107cslzh.cpp',
+                         'pysapcompress/vpa108csulzh.cpp'],
+                        define_macros=[# Enable this macro if you want some debugging information on the (de)compression functions
+                                       #('DEBUG', None),
+                                       # Enable this macro if you want detailed debugging information (hexdumps) on the (de)compression functions
+                                       #('DEBUG_TRACE', None),
+                                       ]
+                        )
 
 
-description = \
-"""
+description = """
 pysap - Python library for communicating using SAP network protocols.
 
 Currently the following SAP protocols are included:
@@ -82,36 +82,32 @@ Currently the following SAP protocols are included:
 """
 
 
-setup(  # Package information
-        name=name,
-        version='0.1.5',
-        author='Martin Gallo',
-        author_email='mgallo@coresecurity.com',
-        description='Python library for crafting SAP\'s network protocols packets',
-        long_description=description,
-        url=url,
-        download_url=url,
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Information Technology',
-            'Intended Audience :: System Administrators',
-            'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-            'Programming Language :: Python',
-            'Programming Language :: C++',
-            'Topic :: Security',
-            ],
+setup(name=name,   # Package information
+      version='0.1.5',
+      author='Martin Gallo',
+      author_email='mgallo@coresecurity.com',
+      description='Python library for crafting SAP\'s network protocols packets',
+      long_description=description,
+      url=url,
+      download_url=url,
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Information Technology',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+                   'Programming Language :: Python',
+                   'Programming Language :: C++',
+                   'Topic :: Security'],
+      # Packages list
+      packages=['pysap'],
+      provides=['pysapcompress', 'pysap'],
 
-        # Packages list
-        packages=['pysap'],
-        provides=['pysapcompress', 'pysap'],
+      # Extension module compilation
+      ext_modules=[sapcompress],
 
-        # Extension module compilation
-        ext_modules=[sapcompress],
+      # Tests command
+      test_suite='tests.suite',
 
-        # Tests command
-        test_suite='tests.suite',
-
-        # API Documentation command
-        cmdclass={'doc': APIDocumentationCommand},
-        )
+      # API Documentation command
+      cmdclass={'doc': APIDocumentationCommand},
+      )
