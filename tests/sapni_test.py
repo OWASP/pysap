7d1dfbc4cbd7e67232ddccfeaf911c0d6fb70fba
Martin Gallo <mgallo@coresecurity.com>
2014-08-25 11:27:32 -0300

    SAPNIStreamSocket tests

--- tests/sapni_test.py
+++ tests/sapni_test.py
@@ -18,18 +18,22 @@
 # ==============
 
 # Standard imports
+import socket
 import unittest
-from struct import unpack
+from threading import Thread
+from struct import pack, unpack
+from SocketServer import BaseRequestHandler, ThreadingTCPServer
 # External imports
-
+from scapy.packet import Packet
+from scapy.fields import StrField
 # Custom imports
-from pysap.SAPNI import SAPNI
+from pysap.SAPNI import SAPNI, SAPNIStreamSocket
 
 
 class PySAPNITest(unittest.TestCase):
 
     def test_sapni(self):
-        """Test SAPNI"""
+        """Test SAPNI length field"""
         test_string = "LALA" * 10
         sapni = SAPNI() / test_string
 
@@ -37,10 +41,135 @@ class PySAPNITest(unittest.TestCase):
         self.assertEqual(sapni_length, len(test_string))
 
 
+class SAPNITestHandler(BaseRequestHandler):
+    """Basic SAP NI echo server"""
+
+    def handle(self):
+        data = self.request.recv(4)
+        (length, ) = unpack("!I", data)
+        data = self.request.recv(length)
+
+        response_length = pack("!I", len(data))
+        self.request.sendall(response_length + data)
+
+
+class SAPNITestHandlerKeepAlive(SAPNITestHandler):
+    """Basic SAP NI keep alive server"""
+
+    def handle(self):
+        self.request.sendall("\x00\x00\x00\x08NI_PING\x00")
+        SAPNITestHandler.handle(self)
+
+
+class PySAPNIStreamSocketBase(unittest.TestCase):
+
+    test_port = 8005
+    test_string = "TEST" * 10
+    handler_cls = SAPNITestHandler
+
+    def setUp(self):
+        self.server = ThreadingTCPServer(("127.0.0.1", self.test_port),
+                                         self.handler_cls,
+                                         bind_and_activate=False)
+        self.server.allow_reuse_address = True
+        self.server.server_bind()
+        self.server.server_activate()
+        self.server_thread = Thread(target=self.server.serve_forever)
+        self.server_thread.start()
+
+    def tearDown(self):
+        self.client.close()
+        self.server.shutdown()
+        self.server.server_close()
+
+
+class PySAPNIStreamSocket(PySAPNIStreamSocketBase):
+
+    def test_sapnistreamsocket(self):
+        """Test SAPNIStreamSocket"""
+        sock = socket.socket()
+        sock.connect(("127.0.0.1", self.test_port))
+
+        self.client = SAPNIStreamSocket(sock)
+        packet = self.client.sr(self.test_string)
+        self.client.close()
+
+        self.assertIn(SAPNI, packet)
+        self.assertEqual(packet[SAPNI].length, len(self.test_string))
+        self.assertEqual(packet.payload.load, self.test_string)
+
+    def test_sapnistreamsocket_base_cls(self):
+        """Test SAPNIStreamSocket handling of custom base packet classes"""
+
+        class SomeClass(Packet):
+            fields_desc = [StrField("text", None)]
+
+        sock = socket.socket()
+        sock.connect(("127.0.0.1", self.test_port))
+
+        self.client = SAPNIStreamSocket(sock, base_cls=SomeClass)
+        packet = self.client.sr(self.test_string)
+        self.client.close()
+
+        self.assertIn(SAPNI, packet)
+        self.assertIn(SomeClass, packet)
+        self.assertEqual(packet[SAPNI].length, len(self.test_string))
+        self.assertEqual(packet[SomeClass].text, self.test_string)
+
+    def test_sapnistreamsocket_getnisocket(self):
+        """Test SAPNIStreamSocket get nisocket class method"""
+
+        self.client = SAPNIStreamSocket.get_nisocket("127.0.0.1",
+                                                     self.test_port)
+
+        packet = self.client.sr(self.test_string)
+        self.client.close()
+
+        self.assertIn(SAPNI, packet)
+        self.assertEqual(packet[SAPNI].length, len(self.test_string))
+        self.assertEqual(packet.payload.load, self.test_string)
+
+
+class PySAPNIStreamSocketKeepAlive(PySAPNIStreamSocketBase):
+
+    handler_cls = SAPNITestHandlerKeepAlive
+
+    def test_sapnistreamsocket_without_keep_alive(self):
+        """Test SAPNIStreamSocket without keep alive"""
+        sock = socket.socket()
+        sock.connect(("127.0.0.1", self.test_port))
+
+        self.client = SAPNIStreamSocket(sock, keep_alive=False)
+        packet = self.client.sr(self.test_string)
+        self.client.close()
+
+        # We should receive a PING instead of our packet
+        self.assertIn(SAPNI, packet)
+        self.assertEqual(packet[SAPNI].length, len(SAPNI.SAPNI_PING))
+        self.assertEqual(packet.payload.load, SAPNI.SAPNI_PING)
+
+    def test_sapnistreamsocket_with_keep_alive(self):
+        """Test SAPNIStreamSocket with keep alive"""
+        sock = socket.socket()
+        sock.connect(("127.0.0.1", self.test_port))
+
+        self.client = SAPNIStreamSocket(sock, keep_alive=True)
+        packet = self.client.sr(self.test_string)
+        self.client.close()
+
+        # We should receive our packet, the PING should be handled by the
+        # stream socket
+        self.assertIn(SAPNI, packet)
+        self.assertEqual(packet[SAPNI].length, len(self.test_string))
+        self.assertEqual(packet.payload.load, self.test_string)
+
+
 def suite():
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
     suite.addTest(loader.loadTestsFromTestCase(PySAPNITest))
+    suite.addTest(loader.loadTestsFromTestCase(PySAPNIStreamSocket))
+    suite.addTest(loader.loadTestsFromTestCase(PySAPNIStreamSocketKeepAlive))
     return suite
 
 
