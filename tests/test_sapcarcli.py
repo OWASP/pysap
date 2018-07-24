af6c2083a07702c842d92a273672b34ebec8d448
Oula Kuuva <oula.kuuva@synopsys.com>
2018-07-24 12:02:34 +0300

    Add tests for PySAPCAR.list method
    Also added testfixtures package to test requirements, as it offers handy
    stuff like logging output checking, which is the correct way of making
    sure the output is correct instead of just checking calls made for
    logger.

--- tests/test_sapcarcli.py
+++ tests/test_sapcarcli.py
@@ -26,6 +26,7 @@ try:
 except ImportError:
     import mock
 
+from testfixtures import LogCapture
 from tests.utils import data_filename
 from pysap.sapcarcli import PySAPCAR
 from pysap.SAPCAR import SAPCARArchive
@@ -35,13 +36,15 @@ class PySAPCARCLITest(unittest.TestCase):
         self.cli = PySAPCAR()
         self.cli.mode = "r"
         self.cli.archive_fd = open(data_filename("car201_test_string.sar"), "rb")
-        self.cli.logger.disabled = True # Mute logging when executing unit tests
+        self.cli.logger.handlers = []  # Mute logging while running unit tests
+        self.log = LogCapture()  # Enable logger output checking
 
     def tearDown(self):
         try:
             self.cli.archive_fd.close()
         except Exception:
             pass
+        self.log.uninstall()
 
     def test_open_archive_fd_not_set(self):
         temp = self.cli.archive_fd  # Backup test archive file handle
@@ -68,6 +71,36 @@ class PySAPCARCLITest(unittest.TestCase):
         names = sorted(["list", "of", "test", "names"])
         targets = sorted(["list", "names", "blah"])
         self.assertEqual(["list", "names"], [n for n in self.cli.target_files(names, targets)])
+
+    def test_list_open_fails(self):
+        with mock.patch.object(self.cli, "open_archive", return_value=None):
+            self.assertIsNone(self.cli.list(None, None))
+
+    def test_list_succeeds(self):
+        archive_attrs = {
+            "files_names": ["test_string.txt", "blah.so"],
+            "files": {
+                "test_string.txt": mock.MagicMock(**{
+                    "permissions": "-rw-rw-r--",
+                    "size": 43,
+                    "timestamp": "01 Dec 2015 22:48",
+                    "filename": "test_string.txt"
+            }),
+                "blah.so": mock.MagicMock(**{
+                    "permissions": "-rwxrwxr-x",
+                    "size": 1243,
+                    "timestamp": "01 Dec 2017 13:37",
+                    "filename": "blah.so"
+                })
+            }
+        }
+        mock_archive = mock.MagicMock(**archive_attrs)
+        with mock.patch.object(self.cli, "open_archive", return_value=mock_archive):
+            self.cli.list(None, None)
+            messages = ("{}  {:>10}    {} {}".format(fil.permissions, fil.size, fil.timestamp, fil.filename)
+                        for fil in archive_attrs["files"].values())
+            logs = (("pysapcar", "INFO", message) for message in messages)
+            self.log.check_present(*logs, order_matters=False)
 
 def test_suite():
     loader = unittest.TestLoader()
