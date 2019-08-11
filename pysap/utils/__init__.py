3c8eae3e645cc0b2f3f8e6be11be47a99bd9fe29
Martin Gallo <mgallo@secureauth.com>
2019-08-11 11:32:44 -0700

    Moved Thread helpers to utils

--- pysap/utils/__init__.py
+++ pysap/utils/__init__.py
@@ -19,6 +19,7 @@
 
 
 # Standard imports
+from Queue import Queue
 from threading import Thread, Event
 
 
@@ -40,3 +41,40 @@ class Worker(Thread):
 
     def stop(self):
         self.stopped.set()
+
+
+# Simple Thread Pool implementation based on http://code.activestate.com/recipes/577187-python-thread-pool/
+# WorkerQueue class
+class WorkerQueue(Thread):
+    """Thread executing tasks from a given tasks queue"""
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try:
+                func(*args, **kargs)
+            except Exception as e:
+                print(e)
+            self.tasks.task_done()
+
+
+# ThreadPool class
+class ThreadPool(object):
+    """Pool of threads consuming tasks from a queue"""
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads):
+            WorkerQueue(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """Add a task to the queue"""
+        self.tasks.put((func, args, kargs))
+
+    def wait_completion(self):
+        """Wait for completion of all the tasks in the queue"""
+        self.tasks.join()
