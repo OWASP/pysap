203788d6a94a563b2748c1913ac4b2410fb80502
Martin Gallo <mgallo@secureauth.com>
2020-06-03 11:49:42 -0700

    Moving to argparse

--- examples/hdb_cli.py
+++ examples/hdb_cli.py
@@ -21,7 +21,7 @@
 # Standard imports
 import logging
 import datetime
-from optparse import OptionParser, OptionGroup
+from argparse import ArgumentParser
 # External imports
 from scapy.config import conf
 # Custom imports
@@ -44,50 +44,42 @@ def parse_options():
 
     description = "This example script is an experimental implementation of the HANA's hdbcli tool."
 
-    epilog = "pysap %(version)s - %(url)s - %(repo)s" % {"version": pysap.__version__,
-                                                         "url": pysap.__url__,
-                                                         "repo": pysap.__repo__}
+    usage = "%(prog)s [options] -d <remote host>"
 
-    usage = "Usage: %prog [options] -d <remote host>"
+    parser = ArgumentParser(usage=usage, description=description, epilog=pysap.epilog)
 
-    parser = OptionParser(usage=usage, description=description, epilog=epilog)
+    target = parser.add_argument_group("Target")
+    target.add_argument("-d", "--remote-host", dest="remote_host",
+                        help="Remote host")
+    target.add_argument("-p", "--remote-port", dest="remote_port", type=int, default=39015,
+                        help="Remote port [%(default)d]")
+    target.add_argument("--route-string", dest="route_string",
+                        help="Route string for connecting through a SAP Router")
+    target.add_argument("--tls", dest="tls", action="store_true",
+                        help="Use TLS/SSL")
 
-    target = OptionGroup(parser, "Target")
-    target.add_option("-d", "--remote-host", dest="remote_host",
-                      help="Remote host")
-    target.add_option("-p", "--remote-port", dest="remote_port", type="int", default=39015,
-                      help="Remote port [%default]")
-    target.add_option("--route-string", dest="route_string",
-                      help="Route string for connecting through a SAP Router")
-    target.add_option("--tls", dest="tls", action="store_true", default=False,
-                      help="Use TLS/SSL [%default]")
-    parser.add_option_group(target)
+    auth = parser.add_argument_group("Authentication")
+    auth.add_argument("-m", "--method", dest="method", default="SCRAMSHA256",
+                      help="Authentication method. Supported methods: {} [%default]".format(",".join(saphdb_auth_methods.keys())))
+    auth.add_argument("--username", dest="username", help="User name")
+    auth.add_argument("--password", dest="password", help="Password")
+    auth.add_argument("--jwt-file", dest="jwt_file", metavar="FILE",
+                      help="File to read a signed JWT from")
+    auth.add_argument("--jwt-cert", dest="jwt_cert", metavar="FILE",
+                      help="File to read the private key to sign the JWT")
+    auth.add_argument("--jwt-issuer", dest="jwt_issuer", help="JWT signature issuer")
+    auth.add_argument("--jwt-claim", dest="jwt_claim", default="user_name",
+                      help="Name of the JWT claim to map username [%(default)s]")
+    auth.add_argument("--saml-assertion", dest="saml_assertion", metavar="FILE",
+                      help="File to read a signed SAML 2.0 bearer assertion from")
+    auth.add_argument("--session-cookie", dest="session_cookie", help="Session Cookie")
+    auth.add_argument("--pid", dest="pid", default="pysap", help="Process ID [%(default)s]")
+    auth.add_argument("--hostname", dest="hostname", help="Hostname")
 
-    auth = OptionGroup(parser, "Authentication")
-    auth.add_option("-m", "--method", dest="method", default="SCRAMSHA256",
-                    help="Authentication method. Supported methods: {} [%default]".format(",".join(saphdb_auth_methods.keys())))
-    auth.add_option("--username", dest="username", help="User name")
-    auth.add_option("--password", dest="password", help="Password")
-    auth.add_option("--jwt-file", dest="jwt_file", metavar="FILE",
-                    help="File to read a signed JWT from")
-    auth.add_option("--jwt-cert", dest="jwt_cert", metavar="FILE",
-                    help="File to read the private key to sign the JWT")
-    auth.add_option("--jwt-issuer", dest="jwt_issuer", help="JWT signature issuer")
-    auth.add_option("--jwt-claim", dest="jwt_claim", default="user_name",
-                    help="Name of the JWT claim to map username [%default]")
-    auth.add_option("--saml-assertion", dest="saml_assertion", metavar="FILE",
-                    help="File to read a signed SAML 2.0 bearer assertion from")
-    auth.add_option("--session-cookie", dest="session_cookie", help="Session Cookie")
-    auth.add_option("--pid", dest="pid", default="pysap", help="Process ID")
-    auth.add_option("--hostname", dest="hostname", help="Hostname")
-    parser.add_option_group(auth)
+    misc = parser.add_argument_group("Misc options")
+    misc.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Verbose output")
 
-    misc = OptionGroup(parser, "Misc options")
-    misc.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
-                    help="Verbose output [%default]")
-    parser.add_option_group(misc)
-
-    (options, _) = parser.parse_args()
+    options = parser.parse_args()
 
     if not options.remote_host:
         parser.error("Remote host is required")
