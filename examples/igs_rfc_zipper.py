790325bcfcb381f6b0512b2d8f31f08b65f78643
Martin Gallo <mgallo@secureauth.com>
2021-07-23 16:49:00 -0700

    Arrange tagline, copyright and license notes across all source files
    Using a more consistent way across projects.

--- examples/igs_rfc_zipper.py
+++ examples/igs_rfc_zipper.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python2
-# ===========
 # pysap - Python library for crafting SAP's network protocols packets
 #
 # SECUREAUTH LABS. Copyright (C) 2021 SecureAuth Corporation. All rights reserved.
-#
-# The library was designed and developed by Martin Gallo from
-# the SecureAuth's Innovation Labs team.
-#
-# Example script by Yvan Genuer
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -18,7 +12,11 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# ==============
+#
+# Author:
+#   Example script by Yvan Genuer (@iggy38).
+#   Martin Gallo (@martingalloar) from SecureAuth's Innovation Labs team.
+#
 
 # Standard imports
 import logging
